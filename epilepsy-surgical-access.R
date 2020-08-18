e5027c672567fb81d55d8f5609819fdd9c874fab
Shreya Louis <slouis.1694@gmail.com>
2020-08-18 18:34:03 -0400

    added R code and csv file

--- epilepsy-surgical-access.R
+++ epilepsy-surgical-access.R
@@ -2,10 +2,11 @@ library(ggplot2)
 library(usmap)
 library(tidyverse)
 
-plot_usmap(regions = "states") + 
+plot_usmap(regions = "states", data = statepop, values = "pop_2015") + 
   labs(title = "Level 4 Epilepsy Centers by State", 
        subtitle = "This is a map depicting epilepsy surgery access based on the prevalence of epilepsy \n per state compared to where Level 4 centers are located") + 
-  theme(panel.background = element_rect(color = "gray", fill = "lightblue"))
+  theme(panel.background = element_rect(color = "gray", fill = "lightblue")) +
+  theme(legend.position = "right")
 
 
 plot_usmap(include = c("OH"), regions = "counties") +
